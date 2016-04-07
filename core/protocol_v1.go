8a66d6e8ac3364ae2c8bd9bbfb75f9a1d862662c
zerak <Kingbug2010@126.com>
2016-04-07 17:44:09 +0800

    protocol v1

--- core/protocol_v1.go
+++ core/protocol_v1.go
@@ -1,12 +1,17 @@
 package core
 
 import (
-	// "bytes"
-	// "encoding/binary"
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"errors"
 	"fmt"
-	"io"
 	"net"
 	"sync/atomic"
+)
+
+const (
+	ProtocolHeaderLen = 9 // header1 + cmd4 + length4
 )
 
 /*
@@ -18,11 +23,11 @@ import (
 
 //   	[x]	   [x][x]      [x][x]  [x][x][x][x]...
 //   | byte | (uint32) |  (uint32)  |   (binary)
-//   |1-byte|  2-byte  |   2-byte   |	 N-byte
+//   |1-byte|  4-byte  |   4-byte   |	 N-byte
 //   --------------------------------------------...
 //    header    cmd	      length         data
-//
-//   [0x05 cmd len data]
+//	 [  0   1234  5678  ...]
+//   [0x05  cmd   len  data]
 */
 type ProtocolV1 struct {
 	ctx *context
@@ -31,82 +36,61 @@ type ProtocolV1 struct {
 func (p *ProtocolV1) IOLoop(conn net.Conn) error {
 	fmt.Printf("[ProtocolV1::Loop] loop ...\n")
 	var err error
-	// var header byte
-	// var cmd uint32
-	// var length int32
+	var header byte
+	var cmd uint32
+	var length int32
 
 	clientId := atomic.AddInt64(&p.ctx.core.clientIDSequence, 1)
 	client := newClient(clientId, conn, p.ctx)
 
-	buf := make([]byte, 8)
-	len := 0
+	buf := make([]byte, ProtocolHeaderLen)
 
 	for {
-		for {
-			// var n int
-			n, err := client.Reader.Read(buf[len:])
-			// n, err := client.Read(buf[len:])
-			fmt.Printf("len[%d] n[%d] err[%v]\n",len, n, err)
-			if err != nil {
-				if err != io.EOF {
-					// HANDLE CLIENT READ ERR
-					fmt.Printf("ProtocolV1 read err-%v\n", err)
-				} else {
-					err = nil
-					len = 0
-				}
-
-				fmt.Printf("ProtocolV1 recv buf[%v]\n", buf)
-				break
+		buf[0] = 0
+		buf, err = client.Reader.Peek(ProtocolHeaderLen)
+		if err != nil {
+			if err == bufio.ErrBufferFull {
+				// no data in the buff continue wait
+				fmt.Printf("ProtocolV1 read err-%v\n", err)
+				continue
 			}
+			fmt.Printf("ProtocolV1 recv buf[%v]\n", buf)
+			break
+		}
 
-			if n > 0 {
-				len += n
-			}
 
-			if n == 0 {
-				break
+		// header
+		header = buf[0]
+		if header != 0x05 {
+			err = fmt.Errorf("ProtocolV1 header[%s] err\n", header)
+			continue
+		}
+
+		// cmd
+		cmd_buf := bytes.NewBuffer(buf[1:5])
+		binary.Read(cmd_buf, binary.BigEndian, &cmd)
+
+		// data
+		len_buf := bytes.NewBuffer(buf[5:9])
+		binary.Read(len_buf, binary.BigEndian, &length)
+		data := make([]byte, ProtocolHeaderLen+length)
+		n, err := client.Reader.Read(data)
+		if err == nil {
+			if (int32)(n) < (length + ProtocolHeaderLen) {
+				fmt.Printf("ProtocolV1 recv[%d] err\n", n, length, data)
+				panic(errors.New("recv bytes not enough"))
 			}
 		}
 
-		// // header
-		// err = binary.Read(conn, binary.BigEndian, &header)
-		// if err != nil {
-		// 	break
-		// }
-		// if header != 0x05 {
-		// 	err = fmt.Errorf("ProtocolV1 header[%s] err\n", header)
-		// 	break
-		// }
-		// fmt.Printf("ProtocolV1 header[%v]\n", header)
+		data = data[ProtocolHeaderLen:]
 
-		// // cmd
-		// err = binary.Read(client.Reader, binary.BigEndian, &cmd)
-		// if err != nil {
-		// 	fmt.Printf("cmd\n")
-		// 	break
-		// }
-		// fmt.Printf("ProtocolV1 cmd[%v]\n", cmd)
+		fmt.Printf("ProtocolV1 header[%v] cmd[%v] len[%d] data[%v]\n", header, cmd, length, data)
 
-		// // data
-		// err = binary.Read(client.Reader, binary.BigEndian, &length)
-		// if err != nil {
-		// 	fmt.Printf("len\n")
-		// 	break
-		// }
-		// data := make([]byte, length)
-		// _, err = io.ReadFull(client.Reader, data)
-		// if err != nil {
-		// 	fmt.Printf("data\n")
-		// 	break
-		// }
-		// fmt.Printf("ProtocolV1 len[%d] data[%v]\n", length, data)
-
-		// err = p.Send(client, []byte("string send to client"))
-		// if err != nil {
-		// 	err = fmt.Errorf("failed to send response - %s", err)
-		// 	break
-		// }
+		err = p.Send(client, []byte("string send to client"))
+		if err != nil {
+			err = fmt.Errorf("failed to send response - %s", err)
+			break
+		}
 
 	} // END CLIENT LOOP
 
@@ -145,4 +129,16 @@ func (p *ProtocolV1) Send(client *ClientV1, data []byte) error {
 	client.writeLock.Unlock()
 
 	return err
+}
+
+func (p *ProtocolV1) readInt(r *bufio.Reader) (int, error) {
+	//b, err := r.ReadBytes(delimEnd)
+	//if err != nil {
+	//	return Resp{}, err
+	//}
+	//i, err := strconv.ParseInt(string(b[1:len(b)-2]), 10, 64)
+	//if err != nil {
+	//	return Resp{}, errParse
+	//}
+	return 1, nil
 }
